158699e40977bc8d4dcf19b2cc18455b369d044e
Omar Mexia Barraza <omar.mexba@gmail.com>
2025-01-26 15:51:42 -0700

    Agregar comentarios

--- Tarea1_ProgramacionDeUnaSolucionParalela/Tarea1_ProgramacionDeUnaSolucionParalela.cpp
+++ Tarea1_ProgramacionDeUnaSolucionParalela/Tarea1_ProgramacionDeUnaSolucionParalela.cpp
@@ -1,8 +1,9 @@
 // Tarea1_ProgramacionDeUnaSolucionParalela.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
-#include <iostream>
+#include <iostream> // Librería estandar para operaciones de entrada y salida
 
+// Solo si OpenMP está disponible se incluye la libreria necesaria para su uso
 #if _OPENMP
     #include <omp.h>
 #endif // _OPENMP
@@ -18,21 +19,38 @@
 /// <param name="b">Arreglo a llenar</param>
 /// <param name="valor">Valor que se usara para generar valores para llenar los arreglos a y b</param>
 void llenarArreglos(float* a, float* b, float valor);
+
+/// <summary>
+/// Imprime los valores del arreglo pasado como parametro
+/// El numero de valores es tomado de la macro mostrar
+/// </summary>
+/// <param name="d"></param>
 void imprimeArreglo(float* d);
 
 int main()
 {
     std::cout << "Sumando Arreglos en Paralelo!\n";
+    // Se declaran los arreglos a sumar (a, b) y el arreglo donde se almacenara el resultado (c)
+    // El tamaño es el mismo para todos con base a la macro definida N
     float a[N], b[N], c[N];
     int i;
 
+    // Se llenan los arreglos a y b.
     llenarArreglos(a, b, 10);
 
+    // se define el numero de particiones en el cual se dividiran los arreglos a sumar
     int pedazos = chunk;
+
+    // Se hace la declaración el comportamiento que nuestro ciclo "for" tendrá
+    // Se establece que los arreglos a, b y c se compartiran entre todas las particiones
+    // la variable i se declara como privada para cada hilo para que su valor permazca sin mezclarse entre ellos
+    // finalmente se establece con la palabra "static" que la particion será establecida de forma fija mediante un numero que es "pedazos"
     #pragma omp parallel for \
     shared(a, b, c, pedazos) private (i) \
     schedule(static, pedazos)
 
+    // Una vez declarado el comportamiento que nuestro ciclo "for" tendrá, se llama de manera convencional
+    // Aunque internamente estará ejecutandose paralelamente con base a las especificaciones establecides con OpenMP
     for (i = 0; i < N; i++)
         c[i] = a[i] + b[i];
 
